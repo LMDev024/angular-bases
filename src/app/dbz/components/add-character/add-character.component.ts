787950b47797fa1cdc3f7a493dfed19e1cd98137
LMDev024 <luismalagondev@gmail.com>
2023-05-14 10:51:26 -0500

    Docs creado

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -18,8 +18,6 @@ export class AddCharacterComponent {
 
   emitCharacter():void{
 
-    console.log(this.character);
-
     if( this.character.name.length === 0 )return;
 
     this.onNewCharacter.emit(this.character);
